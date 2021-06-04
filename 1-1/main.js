72cc0b1778a96abfd2449f5e9063618021ca2b22
ide Shouhi <shouhi0922@icloud.com>
2021-06-04 20:19:56 +0900

    test

--- 1-1/main.js
+++ 1-1/main.js
@@ -1,2 +1,11 @@
 console.log("HelloWorld");
 console.log("HelloWorld");
+
+const arr = ["a", "b", "c"];
+str = "";
+for (let i = 0; i < arr.length; i++) {
+  // console.log(i);
+  str = i;
+}
+
+console.log(str);
